23d2079c12960c05af303ba4595c2058e84f6b86
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-22 19:34:32 +0530

    Added dictionaries files to DAY-03 playground.

--- DAY-03/MyPlayground.playground/Contents.swift
+++ DAY-03/MyPlayground.playground/Contents.swift
@@ -1,5 +1,5 @@
 // DataTypes: Arrays, Dictionaries, Sets, Enums.
-
+// ARRAYS: elements can be stored in the order we add them and can have duplicates.
 // How to store ordered data in Arrays.
 
 var marvelHeroes = ["Thor", "Black Widow", "Captain America"]           // array of strings.
@@ -54,5 +54,45 @@ print(planets.contains("Earth"))
 
 print(planets.sorted())
 
-// Dictionaries.
+// Dictionaries: don't allow duplicate keys, if we set a value for a key that already exists, swift overwrites the previous assigned value.
 // How to store & find data in dictionaries.
+ 
+let employee = [
+    "name": "Himanshu",
+    "job": "BankPO",
+    "location": "Delhi"
+]
+// In case of dictionaries, always provide the default values, in case the desired element was not found, default value will be shown as the output.
+print(employee["name", default: "Unknown"])
+print(employee["job", default: "Unknown"])
+print(employee["location", default: "Unknown"])
+
+var hasGraduated = [
+    "CaptainAmerica": false,
+    "WonderWomen": true,
+    "Batman": true,
+    "IronMan": true,
+    "Thor": false
+]
+// NOTE: default values has to be the same type like key datatype.
+print(hasGraduated["CaptainAmerica", default: false])
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+print(olympics[2012, default: "Unknown"])
+
+// We can create an empty dictionary using explicit types, we want to store.
+var height = [String: Int]()
+height["Lebron James"] = 206
+height["Micheal Phelps"] = 205
+height["Micheal Jordan"] = 208
+print(height)
+print(height["Lebron James", default: 0])
+
+// some dictionaries methods.
+print(height.count)
+print(height.keys)
+print(height.values)
