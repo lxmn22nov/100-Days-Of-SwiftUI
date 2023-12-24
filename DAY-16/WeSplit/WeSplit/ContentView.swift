8188ae73a82d2f4da898c8afb3510f237799e120
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-24 23:58:19 +0530

    Added Navigation files.

--- DAY-16/WeSplit/WeSplit/ContentView.swift
+++ DAY-16/WeSplit/WeSplit/ContentView.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
+    
     var body: some View {
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
+        }
         NavigationStack {
             Form {
-                Text("Hello, Swift.")
-                Text("I'm Laxman Singh Koranga.")
-                Text("I'm iOS Dev.")
+//                Text("Hello, Swift.")
+//                Text("I'm Laxman Singh Koranga.")
+//                Text("I'm iOS Dev.")
                 Section {
                     Text("Hello, Swift.")
                     Text("I'm Laxman Singh Koranga.")
                     Text("I'm iOS Dev.")
                 }
-                Section {
-                    Text("I'm Laxman Singh Koranga.")
-                }
-                Section {
-                    Text("I'm iOS Dev.")
-                }
+//                Section {
+//                    Text("I'm Laxman Singh Koranga.")
+//                }
+//                Section {
+//                    Text("I'm iOS Dev.")
+//                }
             }
             .navigationTitle("SwiftUI")
             .navigationBarTitleDisplayMode(.inline)
