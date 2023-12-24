a09bc91710708be9f06776be0df986385f281863
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-24 18:16:16 +0530

    Added break and continue keyword files.

--- DAY-06/MyPlayground.playground/Contents.swift
+++ DAY-06/MyPlayground.playground/Contents.swift
@@ -49,4 +49,28 @@ print("BlastOff!")
 
 // NOTE: The main difference is that for loops are generally used with finite sequences: we loop through the numbers 1 through 10, or through the items in an array. On the other hand, while loops can loop until any arbitrary condition becomes false, which allows them until we tell them to stop.
 
+// Break and Continue in Loops: "continue" skips the current loop iteration and "break" skips all remaining iterations.
 
+let fileNames: [String] = ["me.jpg","work.txt","sophie.jpg","logo.png"]
+
+for fileName in fileNames {
+    if fileName.hasSuffix(".jpg") == false {
+        continue
+    }
+    print("Found picture: \(fileName)")
+}
+
+let num1: Int = 2
+let num2: Int = 10
+var multiples = [Int]()
+
+for i in 1...100 {
+    if i.isMultiple(of: num1) && i.isMultiple(of: num2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+print(multiples)
