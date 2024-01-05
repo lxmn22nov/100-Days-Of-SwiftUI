fe9e0720295f74aae4f61f007981669b5dd33e36
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-05 22:20:34 +0530

    Added files to DAY-17.

--- DAY-17/WeSplit/WeSplit/ContentView.swift
+++ DAY-17/WeSplit/WeSplit/ContentView.swift
@@ -11,8 +11,18 @@ struct ContentView: View {
     @FocusState private var amountIsFocused: Bool
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
-    @State private var tipPercentage: Int = 20
-    let tipPercentages = [0, 10, 15, 20, 25]
+    @State private var tipPercentage: Int = 5
+//    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    // totalAmount without split.
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = ((checkAmount / 100) * tipSelection)
+        let grandTotal = (checkAmount + tipValue)
+        
+        return grandTotal
+    }
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
@@ -41,16 +51,22 @@ struct ContentView: View {
                 }
                 Section("How much do you want to tip?") {
                     Picker("Tip Percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
+//                        ForEach(tipPercentages, id: \.self) {
+//                            Text($0, format: .percent)
+//                        }
+                        ForEach(0..<101) {
+                            Text("\($0)%")
                         }
                     }
-                    .pickerStyle(.segmented)
+//                    .pickerStyle(.segmented)
                 }
                 
                 //            .pickerStyle(.navigationLink)
                 
-                Section("Individual have to pay:") {
+                Section("Total amount for the check") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
