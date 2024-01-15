65a4f12c008cdba8d017d119fd13d965a0509535
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-15 21:50:39 +0530

    Added files to DAY-26.

---
+++ DAY-26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Laxman Singh Koranga on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
