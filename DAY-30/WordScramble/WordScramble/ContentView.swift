bd59d544c3b4dff1591a3d35495c28ce24882703
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-18 00:02:21 +0530

    Added files to DAY-30.

---
+++ DAY-30/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Laxman Singh Koranga on 17/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never )
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+            
+        // extra validation to come.
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+#Preview {
+    ContentView()
+}
