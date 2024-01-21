a82ab0c88563c2291744b9c96e90c86214b3854f
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-22 02:44:49 +0530

    Added file to DAY-32.

---
+++ DAY-32/Animation/Animation/AnimationView.swift
@@ -0,0 +1,36 @@
+//
+//  AnimationView.swift
+//  Animation
+//
+//  Created by Laxman Singh Koranga on 22/01/2024.
+//
+
+import SwiftUI
+
+struct AnimationView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)), in: 1...10)
+            Spacer()
+            Button("Tap Me.") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AnimationView()
+}
