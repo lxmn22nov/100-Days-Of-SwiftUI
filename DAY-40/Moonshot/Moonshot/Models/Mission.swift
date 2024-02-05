64b7811476fd7bf880a8ed71317e51314c8f8cc0
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-02-06 01:31:19 +0530

    Added updated files to DAY-40.

--- DAY-40/Moonshot/Moonshot/Models/Mission.swift
+++ DAY-40/Moonshot/Moonshot/Models/Mission.swift
@@ -8,18 +8,24 @@
 import Foundation
 
 struct Mission: Codable, Identifiable {
-    // Nested Struct. 
+    // Nested Struct.
     struct CrewRole: Codable {
         let name: String
         let role: String
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     
-    let displayName: String {
+    var displayName: String {
         "Apollo \(id)"
+    }
+    var image: String {
+        "apollo\(id)"
+    }
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
 }
