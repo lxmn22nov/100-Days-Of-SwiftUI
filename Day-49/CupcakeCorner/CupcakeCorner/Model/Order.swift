5220b1c2a9a3228ba84ff4dbda9ff68d986a846e
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-03-04 16:41:55 +0530

    Added updated files.

--- Day-49/CupcakeCorner/CupcakeCorner/Model/Order.swift
+++ Day-49/CupcakeCorner/CupcakeCorner/Model/Order.swift
@@ -26,4 +26,37 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var pinCode = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || pinCode.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    // pricing of cakes, computed property.
+    var cost: Double {
+        // 160 Rs per cake.
+        var cost = Double(quantity) * 160
+        
+        // complicated cakes cost more.
+        cost += (Double(type) / 2)
+        
+        // 80/cake for extra frosting.
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        
+        // 40/cake for sprinkles.
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+        
+        return cost
+    }
 }
