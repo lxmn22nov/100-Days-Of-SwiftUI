4fe95b9f255dc78eb8fc5e713f55d4a5a7f50f4a
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-03-11 21:28:10 +0530

    Added view file to DAY-53.

---
+++ DAY-53/Bookworm/Bookworm/Views/ContentView3.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView3.swift
+//  Bookworm
+//
+//  Created by Laxman Singh Koranga on 11/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView3()
+}
