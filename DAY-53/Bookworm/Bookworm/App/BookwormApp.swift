1848547a40196b4cf1dc81646df0d8a87a1d40d2
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-03-12 09:40:30 +0530

    Added updated files.

--- DAY-53/Bookworm/Bookworm/App/BookwormApp.swift
+++ DAY-53/Bookworm/Bookworm/App/BookwormApp.swift
@@ -5,13 +5,17 @@
 //  Created by Laxman Singh Koranga on 11/03/2024.
 //
 
+import SwiftData
 import SwiftUI
 
+// @main: tells Swift this is what launches our app, from the iOS home scree.
+// WindowGroup: tells SwiftUI our app can be displayed in many windows. This doesn't do much on iPhone, but on iPad and macOS.
 @main
 struct BookwormApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView3()
         }
+        .modelContainer(for: Student.self)
     }
 }
--- DAY-53/Bookworm/Bookworm/Views/ContentView3.swift
+++ DAY-53/Bookworm/Bookworm/Views/ContentView3.swift
@@ -5,11 +5,34 @@
 //  Created by Laxman Singh Koranga on 11/03/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView3: View {
+    // accessing the model context that was created earlier.
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(students) { student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar {
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasly"]
+                    
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    // created student object.
+                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                    // we need to ask modelContext to add that student, which means it'll be saved.
+                    modelContext.insert(student)
+                }
+            }
+        }
     }
 }
 
