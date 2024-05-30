186e184236c9d66077b94d4b9187292e753dedf8
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-05-30 22:14:55 +0530

    Added views to DAY-62/InstaFilter.

---
+++ DAY-62/Instafilter/Instafilter/VIews/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Laxman Singh Koranga on 28/05/2024.
+//
+
+// Objective: The basic user interface.
+// Objective: Importing an image into SwiftUI using PhotosPicker.
+// Objective: Basic image filtering using Core Image.
+
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
+import SwiftUI
+
+struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+     
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
+        }
+    }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            // more code
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+}
+
+#Preview {
+    ContentView()
+}
